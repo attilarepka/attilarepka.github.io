41a1fecbe57fa3f933bbac4c9ac68f73122cb6c1
Attila Repka <repka.attila@yandex.com>
2020-05-02 14:44:02 +0200

    Updates

---
+++ precache-manifest.249282debc5112634e9d6b0d3a5342f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ce0178ed6af7b2f2b46979a46244e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96d60b4d6f95255c9245",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9045de79a36ae50807d9",
+    "url": "/static/js/2.069b31ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.069b31ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d60b4d6f95255c9245",
+    "url": "/static/js/main.eda460c8.chunk.js"
+  },
+  {
+    "revision": "1a11b0b82f4817fa600a",
+    "url": "/static/js/runtime-main.a19d19f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
